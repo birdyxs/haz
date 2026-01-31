3a679a4aa47376d436463e29c833a1a07c2bc913
mar <105406641+birdyxs@users.noreply.github.com>
2026-01-31 09:06:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const statusBtn = document.querySelector('.btn-status');
+
+function controlarEstadoChile() {
+    const opciones = { timeZone: 'America/Santiago', hour: 'numeric', hour12: false };
+    const horaChile = parseInt(new Intl.DateTimeFormat('en-US', opciones).format(new Date()));
+
+
+    const horaApertura = 9;
+    const horaCierre = 19;
+
+    if (horaChile >= horaApertura && horaChile < horaCierre) {
+        statusBtn.classList.add('status-online');
+        statusBtn.classList.remove('status-offline');
+        statusBtn.title = "Online (Horario Chile)";
+    } else {
+        statusBtn.classList.add('status-offline');
+        statusBtn.classList.remove('status-online');
+        statusBtn.title = "Offline (Fuera de horario)";
+    }
+}
+
+
+controlarEstadoChile();
+
+setInterval(controlarEstadoChile, 60000);
